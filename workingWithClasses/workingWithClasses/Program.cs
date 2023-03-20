e4631d9c7fe8b819e349282426c78897ef699d34
DzhangrErdniev21D <85980107+KalmDzhan@users.noreply.github.com>
2023-03-20 22:30:39 +0300

    Add files via upload

---
+++ workingWithClasses/workingWithClasses/Program.cs
@@ -0,0 +1,15 @@
+﻿using workingWithClasses;
+
+Subject[] grades =
+{
+    new Subject("МДК 01.01", true, 5),
+    new Subject("МДК 04.01", false, 2),
+    new Subject("МДК 04.02", false, 2),
+    new Subject("МДК 11.01", true, 5),
+    new Subject("Основы алгоритмизации", true, 5),
+};
+
+foreach (Subject subject in grades)
+{
+    Console.WriteLine(subject.ToString());
+}
---
+++ workingWithClasses/workingWithClasses/Subject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workingWithClasses
+{
+    public class Subject
+    {
+        public Subject() { }
+        public Subject(string subjectName, bool attestation, int grade)
+        {
+            SubjectName = subjectName;
+            Attestation = attestation;
+            Grade = grade;
+        }
+        public string SubjectName { get; set; }
+        public bool Attestation { get; set; }
+        public int Grade { get; set; }
+
+        public override string ToString()
+        {
+            return "Предмет: " + SubjectName + " |Аттестация: " + Attestation + " |Оценка♥: " + Grade;
+        }
+    }
+}
